7362b1b32f9eab27327919b0ffc341e2f1d55db4
Eve Weinberg <evejweinberg@gmail.com>
2015-11-12 02:31:07 -0500

    added silly audio

--- SuperHero_v2/sketch.js
+++ SuperHero_v2/sketch.js
@@ -1,11 +1,11 @@
-var earthorbit, inst;
+var earthorbit, inst, saveme, bg01, asteroid, scene3header, flapTemp, dearEarth;
 var instscaledown = 300;
 var earthorbit_frames = [];
 var aftercape_frames = [];
 var currentframeAF = 0;
 var currentframe = 0;
 var totalearthorbitframes = 320;
-var totalaftercapeframes = 54;
+var totalaftercapeframes = 326;
 var aspect = 1920 / 1080;
 var aftercape;
 var playSecondVid = false;
@@ -13,11 +13,19 @@ var instructionsready = false;
 var strokevar = 1;
 var particleList = [];
 var singlestar = [];
+var savmeplaying = false;
+var bgplaying = false;
+var dearEarthplaying = false;
+var ast_x = -100;
+var ast_y = -100;
+  var scene3counter = 0;
 
 var totalstars = 20;
+var scene1 = true;
 var scene2 = false;
 var scene3 = false;
-var scene1 = true;
+var scene4 = false;
+
 
 function starfield1() {
   this.x = random(1920);
@@ -36,6 +44,14 @@ function starfield1() {
     } else if (this.radius < 5) {
       this.radius = this.radius;
     }
+  }
+}
+
+function playSaveMe() {
+  if (!savmeplaying) {
+    saveme.play();
+    savmeplaying = true;
+
   }
 }
 
@@ -62,58 +78,110 @@ function preload() {
     earthorbit = "assets/EarthOrbit" + nf(i, 3) + ".png";
     earthorbit_frames.push(loadImage(earthorbit)); //push them all into an array
 
-    //earthPNGSeq.img.push(loadImage(earthorbit));
   }
   for (var i = 0; i < totalaftercapeframes; i++) { //load all the image names
-    aftercape = "assets/AC" + nf(i, 3) + ".png";
+    aftercape = "assets/AC2_" + nf(i, 3) + ".png";
     aftercape_frames.push(loadImage(aftercape)); //push them all into an array
   }
+  saveme = loadSound('assets/saveme.m4a');
+  bg01 = loadSound('assets/bg01.mp3');
+  dearEarth = loadSound('assets/dearEarth.m4a');
+  asteroid = loadImage('assets/asteroid.png');
 }
 
 
 
+function dearEarthVO() {
+  if (!dearEarthplaying) {
+    dearEarth.setVolume(0.6);
+    dearEarth.play();
+    dearEarthplaying = true;
+    bg01.setVolume(0.05);
+  }
+}
 
+function bgmusic() {
+  if (!bgplaying) {
+    bg01.setVolume(0.1);
+    bg01.play();
+    bgplaying = true;
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  flapTemp = createImg('assets/flapTemp.gif');
+  var   flapTemp2 = createImg('assets/flapTemp.gif');
+  var   flapTemp3 = createImg('assets/flapTemp.gif');
+  flapTemp.position(100, 300).class('class3');
+  flapTemp2.position(800, 300).class('class3');
+  flapTemp3.position(1400, 300).class('class3');
+  $('.header').hide();
   inst = createImg('assets/getinstructions.png');
   inst.class('instructions');
+  scene3header = createP('we need to test your flight skills, pronto');
+  scene3header.class('header').class('class3');
+  $('.class3').hide();
+
 
   for (var m = 0; m < totalstars; m++) {
     singlestar.push(new starfield1());
   }
 } ///SETUP ENDS
 
+function checkFlap1(){
+  // if return true, call checkFlap2();
+  //ect
+
+  console.log('scene3counter:' +scene3counter)
+  scene3counter++;
+  if (scene3counter > 600){
+    scene4 = true;
+  }
+  
+}
+
 
 function draw() {
+  bgmusic();
   clear();
-
-  if (scene3 == true) {
-    ellipse(30, 30, 130, 130);
-
-  }
-
-  if (scene2 == true) {
+if(scene4 == true){
+  $('.class3').hide();
+  scene3 = false;
+  dearEarthVO();
+  background(236,115,105);
+  
+}
+  else if (scene3 == true) {
+    $('.class3').show();
+    checkFlap1();
+    
+  } else if (playSecondVid == true) {
+    scene2 = true;
+    scene1 = false;
+    console.log("scene2=true");
+    scene1 = false;
     for (var o = 0; o < totalstars; o++) {
       singlestar[o].display();
       singlestar[o].twinkle();
     }
-  }
+    if (currentframeAF == 57) {
+      playSaveMe();
+    }
+    if (currentframeAF > 87) {
+      var targetastx = 600;
+      var targetasty = 600;
+      image(asteroid, ast_x + random(-3, 3), ast_y + random(-3, 3));
+      ast_x = abs(targetastx - ast_x) * .05;
 
-
-
-  if (playSecondVid == true) {
-    scene2 = true;
+    }
     if (currentframeAF < totalaftercapeframes) {
       image(aftercape_frames[currentframeAF++], 0, 0, windowWidth, windowWidth / aspect);
     } else {
       image(aftercape_frames[currentframeAF - 1], 0, 0, windowWidth, windowWidth / aspect);
       loadfirstinstruction();
     }
-
   }
-
-
 }
 
 function keyPressed() {
@@ -121,13 +189,15 @@ function keyPressed() {
   if (scene2 === true) {
     if (keyCode === ENTER) {
       console.log('scene2end');
-      scene3 == true;
-      scene2 == false;
-      playSecondVid == false;
+      scene3 = true;
+      scene2 = false;
+      playSecondVid = false;
     }
-  }
-
-  if (scene1 === true) {
+    //   if (keyCode === 66) {
+    //   console.log('b was pressed');
+    //   animateInsttocorner();
+    // }
+  } else if (scene1 === true) {
     if (keyCode === ENTER) {
       console.log('scene1end');
       EndIntro();
@@ -136,31 +206,19 @@ function keyPressed() {
       scene2 == true;
 
     }
-  }
-
-  if (key === ' ') {
+  } else if (key === ' ') {
     print('pressed space');
     currentframe++;
     image(earthorbit_frames[currentframe], 0, 0, windowWidth, windowWidth / aspect);
   }
-
-
   // return false;
 
-  if (keyCode === 66) {
-    console.log('b was pressed');
-    animateInsttocorner();
-  }
-  if (keyCode === 80) {
-
-  }
 }
 
 
 
 function loadfirstinstruction() {
   $('.instructions').show();
-
 }
 
 function animateInsttocorner() {
@@ -180,10 +238,9 @@ function resize() {
 }
 
 
-
-
 function EndIntro() {
   if ($('#loading').is(':visible')) {
     $('#loading').hide();
   }
+  scene1 = false;
 }
