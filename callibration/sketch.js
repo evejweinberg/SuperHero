9c4dbde26352239293b07cb1717ced0fc5d083ec
Eve Weinberg <eve@neveroddoreven.tv>
2015-11-21 13:14:03 -0500

    callibration code is done :)

--- callibration/sketch.js
+++ callibration/sketch.js
@@ -9,58 +9,88 @@ var sliderTemp; //the sensor will replace this later
 var restingNumbers = [];
 var isCallibrationReady = [];
 var NumstoCallibrate = [];
-var readingVal, gd;
+var SensorVal, gd;
 var distanceofvalues = 0;
 var averageValpreCal = 0;
 var sum = 0;
 var callibrationPreStage = true;
 var callibrationStage = false;
+var scene1 = false;
 var timer;
+var CallibratedRestingNum = 0;
+var callibrationCountdown = 5;
+var calcountdown;
 
 function setup() {
   createCanvas(700, 700);
-  gd = new getReadingChange();
+  gd = new getSensorValChange();
   sliderTemp = createSlider(500, 600, 550); //the sensor will replace this later
 
-  timer = setTimeout(function(){
-    console.log("fire togglestages")
+  timer = setTimeout(function() {
+    togglestages();
   }, 3000);
 
 }
 
 function draw() {
   background(255);
-  textSize(30);
-  text('raw value     ' + readingVal, 40, 60);
-  text('distance     ' + distanceofvalues, 40, 80);
-  text('average     ' + averageValpreCal, 40, 100);
+  textSize(20);
+  text('raw value     ' + SensorVal, 40, 60);
+  text('fluctuation     ' + distanceofvalues, 40, 80);
+
   // console.log('sum is     ' + sum);
-  // console.log('calibrationReading' + isCallibrationReady);
-  // console.log(readingVal);
+  // console.log('calibrationSensorVal' + isCallibrationReady);
+  // console.log(SensorValVal);
   gd.display();
-  readingVal = sliderTemp.value();
+  SensorVal = sliderTemp.value();
 
 
+  if (scene1 == true) {
+    background(0);
+    fill(255, 0, 0);
+    text('GAME ON  ', 40, 40);
+  }
 
 
   if (callibrationStage === true) {
-      text('CALLIBRATING! HOLD STEADY', 40, 40);
-      NumstoCallibrate.push(distanceofvalues);
-    if (NumstoCallibrate.length > 10) { // write over the 10 numbers in the array
+    text('CALLIBRATING! HOLD STEADY FOR:  ' + callibrationCountdown, 40, 40);
+
+    // var calcountdown = setTimeout(function() {
+    //   console.log(callibrationCountdown);
+    //   Math.round(callibrationCountdown--);
+
+    // }, 1000);
+    // clearTimeout ( calcountdown );
+
+    // if (millis()%1000==0){
+    //   console.log(callibrationCountdown);
+    // round(callibrationCountdown--);
+    // }
+    if (callibrationCountdown <= 0) {
+      togglestages2();
+      window.clearInterval(calcountdown);
+    }
+    text('resting value    ' + CallibratedRestingNum, 40, 150);
+    NumstoCallibrate.push(SensorVal); //now that we're steady, lets gather the actual number
+    if (NumstoCallibrate.length > 100) { // write over the 100 numbers in the array
       NumstoCallibrate.splice(0, 1);
     }
     sum = 0;
-    for (var i = 0; i < NumstoCallibrate.length; i++) {
-      var num = Number(NumstoCallibrate[i]);
-      sum = sum + num;
+    for (var i = 0; i < NumstoCallibrate.length; i++) { //100 times
+      var num = Number(NumstoCallibrate[i]); //raw sensor numbers
+      sum = sum + num; //add them all up
     }
-    averageValpreCal = sum / NumstoCallibrate.length;
-      
+    CallibratedRestingNum = sum / NumstoCallibrate.length;
+
 
   }
 
   if (callibrationPreStage === true) {
-    text('START CALLIBRATION', 40, 40);
+    text('STARTING CALLIBRATION', 40, 40);
+    if (distanceofvalues > 6) {
+      text("YOU'RE NOT STEADY ENOUGH", 280, 180);
+    }
+    text('average fluctuation     ' + averageValpreCal, 40, 100);
     isCallibrationReady.push(distanceofvalues);
     if (isCallibrationReady.length > 10) { // write over the 10 numbers in the array
       isCallibrationReady.splice(0, 1);
@@ -75,36 +105,46 @@ function draw() {
 
 
   if (averageValpreCal > 6) {
-    
+
     window.clearTimeout(timer)
-    
-    timer = setTimeout(function(){
+
+    timer = setTimeout(function() {
       console.log("fire togglestages")
       togglestages();
     }, 3000);
-    
-    
-    
+
+
+
   }
 } //draw ends
 
 function togglestages() {
   if (callibrationPreStage == true) {
-    
+
     callibrationPreStage = false;
     callibrationStage = true;
+    Begincountdown();
   }
+
+
 
 }
 
-function getReadingChange() {
-  this.currentVal = readingVal;
+function togglestages2() {
+  if (callibrationStage == true) {
+    callibrationStage = false;
+    scene1 = true;
+  }
+}
+
+function getSensorValChange() {
+  this.currentVal = SensorVal;
   console.log('cv' + this.currentVal);
-  this.previousVal = readingVal;
+  this.previousVal = SensorVal;
   this.lastcheck = 0;
 
   this.display = function() {
-    this.currentVal = readingVal;
+    this.currentVal = SensorVal;
 
     if (millis() - this.lastcheck > 100) { //read every 10th of a second
       distanceofvalues = abs(this.currentVal - this.previousVal);
@@ -113,4 +153,11 @@ function getReadingChange() {
       this.lastcheck = millis();
     }
   }
+}
+
+function Begincountdown() {
+  calcountdown = window.setInterval(function() {
+    console.log(callibrationCountdown);
+    Math.round(callibrationCountdown--);
+  }, 1000);
 }
